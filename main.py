7a54ddc00ed9ca7391253db380e9dc2e6618acf8
pgraciac <pgraciacorrea@gmail.com>
2022-03-24 12:31:21 +0100

    nada

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    legal_df = pd.read_json('/Logs/legal.json')
-    legal_df.head()
-    users_df = pd.read_json('/Logs/users.json')
-    users_df.head()
+    legal_df = pd.read_json('/Logs/legal.json', orient = 'records')
+    print(legal_df)
+
