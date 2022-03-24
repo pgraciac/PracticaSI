8bc4e70bc86bdb5ae1e21bacaa33098bed646bc3
pgraciac <pgraciacorrea@gmail.com>
2022-03-24 18:34:51 +0100

    Tablas perfectas + recuperar de DB

--- basedatos.py
+++ basedatos.py
@@ -1,14 +1,30 @@
 import sqlite3
+import pandas as pd
+
+
+def getFromDB(table):
+    con = sqlite3.connect('PracticaSistemas.db')
+    df = pd.read_sql_query(f"SELECT * FROM {table}", con)
+    con.commit()
+    con.close()
+    return df
 
 
 def insertarLegal(web):
     con = sqlite3.connect('PracticaSistemas.db')
     cur = con.cursor()
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS legal(name text primary key, cookies int, aviso int, proteccion_de_datos int, creacion int)")
+        "CREATE TABLE IF NOT EXISTS legal(name text primary key, cookies int, aviso int, proteccionDeDatos int, creacion int)")
     cur.execute(
         f"INSERT INTO legal VALUES('{list(web.keys())[0]}', {(web[list(web.keys())[0]])['cookies']}, {(web[list(web.keys())[0]])['aviso']},"
         f"{(web[list(web.keys())[0]])['proteccion_de_datos']},{(web[list(web.keys())[0]])['creacion']})")
+    con.commit()
+    con.close()
+
+def borrarFecha():
+    con = sqlite3.connect('PracticaSistemas.db')
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS fecha")
     con.commit()
     con.close()
 
@@ -24,8 +40,9 @@ def borrarLegal():
 def insertarUsers(user):
     con = sqlite3.connect('PracticaSistemas.db')
     cur = con.cursor()
-    cur.execute("CREATE TABLE If NOT EXISTS users(name text primary key, telefono int , contrasena text, provincia text,"
-                " permisos int, totalemails int, phisingemails int, clicadosemails int, totalfechas int, totalips int)")
+    cur.execute(
+        "CREATE TABLE If NOT EXISTS users(name text primary key, telefono int , contrasena text, provincia text,"
+        " permisos int, totalEmails int, phisingEmails int, clicadosEmails int, totalFechas int, totalIps int)")
 
     if (user[list(user.keys())[0]])['telefono'] != 'None':
         telefono = (user[list(user.keys())[0]])['telefono']
@@ -41,6 +58,12 @@ def insertarUsers(user):
                 f"{(user[list(user.keys())[0]])['permisos']}, {((user[list(user.keys())[0]])['emails'])['total']},"
                 f"{((user[list(user.keys())[0]])['emails'])['phishing']}, {((user[list(user.keys())[0]])['emails'])['cliclados']},"
                 f"{len((user[list(user.keys())[0]])['fechas'])}, {len((user[list(user.keys())[0]])['ips'])})")
+    cur.execute("CREATE TABLE IF NOT EXISTS fecha(fecha text,name text, foreign key(name) references users(name))")
+    for fecha in (user[list(user.keys())[0]])['fechas']:
+        cur.execute(f"INSERT INTO fecha VALUES('{fecha}', '{list(user.keys())[0]}')")
+    cur.execute("CREATE TABLE IF NOT EXISTS ip(ip text,name text, foreign key(name) references users(name))")
+    for ip in (user[list(user.keys())[0]])['ips']:
+        cur.execute(f"INSERT INTO ip VALUES('{ip}', '{list(user.keys())[0]}')")
     con.commit()
     con.close()
 
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import pandas as pd
 from legal import legalToDB
 from users import usersToDB
-from basedatos import borrarLegal, borrarUsers
+from basedatos import borrarLegal, borrarUsers, getFromDB, borrarFecha
+import pandas as pd
 
 if __name__ == '__main__':
     borrarLegal()
     borrarUsers()
+    borrarFecha()
     legalToDB()
     usersToDB()
+    #dfLegal = getFromDB('legal')
+    #dfUsers = getFromDB('users')
+    #print(dfUsers)
+    #print(dfLegal)
+
