ac2f5daaf5221acca65d36ea268984d0fc152282
pgraciac <pgraciacorrea@gmail.com>
2022-03-26 15:35:01 +0100

    Mas del 4

--- MIS.py
+++ MIS.py
@@ -69,12 +69,12 @@ if __name__ == '__main__':
     ejex = []
     ejey = []
     while len(webDesactualizadas) < 5:
-        max = dfDesactualizados['desact'].max()
-        dfAux = dfDesactualizados[dfDesactualizados['desact'] == max]
+        min = dfDesactualizados['desact'].min()
+        dfAux = dfDesactualizados[dfDesactualizados['desact'] == min]
         dfAux = dfAux.sort_values('creacion')
         name = list(dfAux['name'].head(1))[0]
         ejex.append(name)
-        ejey.append(max)
+        ejey.append(min)
         webDesactualizadas.append(name)
         dfDesactualizados = dfDesactualizados.drop(dfDesactualizados[dfDesactualizados['name']==name].index)
     print("Las 5 web más desactualizadas son:",end=" ")
@@ -84,10 +84,38 @@ if __name__ == '__main__':
         else:
             print(web, end=", ")
     parameters = {'axes.labelsize': 10,
-                  'xtick.labelsize':7}
+                  'xtick.labelsize': 6}
+    plt.figure()
     plt.rcParams.update(parameters)
-    plt.bar(ejex,ejey)
+    plt.bar(ejex, ejey)
     plt.ylabel("Cantidad de políticas desactualizadas")
     plt.xlabel("Nombre de la web")
     plt.title('Las 5 webs más desactualizadas')
+    plt.show()
+
+    anos = sorted(dfLegal['creacion'].unique().tolist())
+    print(anos)
+    siPoli = [0] * len(anos)
+    noPoli = [0] * len(anos)
+    i = 0
+    for ano in anos:
+        dfAux = dfLegal[dfLegal['creacion'] == ano]['proteccionDeDatos']
+        for aux in list(dfAux):
+            if aux == 0:
+                noPoli[i] += 1
+            else:
+                siPoli[i] += 1
+        i += 1
+    print(siPoli,noPoli)
+    plt.figure()
+    i = 0
+    ancho = 0.4
+    barraPoli = plt.bar(anos, siPoli, ancho,  label='Tiene política')
+    anosb = anos.copy()
+    for ano in anosb:
+        anosb[i] += ancho
+        i += 1
+    print(anosb)
+    barraNoPoli = plt.bar(anosb, noPoli, ancho, label='No tiene política')
+    plt.legend(loc='best')
     plt.show()
