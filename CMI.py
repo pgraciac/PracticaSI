cf88fda3751022bd1d3e71bc8f8c1227ef1a90c1
pgraciac <pgraciacorrea@gmail.com>
2022-04-27 15:45:43 +0200

    ejercicio 2 sin formatear

--- CMI.py
+++ CMI.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template
 
+from basedatos import getFromDB
+from users import criticUsers
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '<p>Hello, World!</p>'
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name = None):
-    return render_template('hello.html', name = name)
+@app.route('/ejercicio2/')
+def ejercicio2():
+    dfLegal = getFromDB('legal')
+    dfUsers = getFromDB('users')
+    users = criticUsers(dfUsers)[0]
+    return render_template('ejercicio2.html', users = users)
 
 if __name__ == '__main__':
     app.run(debug=True)
