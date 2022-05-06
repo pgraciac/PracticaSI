dab4c3a1531dc4587ae331c6376b807a74ff2d07
pgraciac <pgraciacorrea@gmail.com>
2022-05-06 19:47:51 +0200

    casi regresion lineal
    falta mostrar

--- ia.py
+++ ia.py
@@ -1,38 +1,38 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 import json
-
-with open("Logs/users_IA_clases.json") as usersjson:
-    users = json.load(usersjson).get('usuarios')
-print(users)
-usuarios_train_x = []
-usuarios_train_y = []
-for user in users:
-    if user.get('emails_phishing_clicados') != 0:
-        usuarios_train_x.append(user.get('emails_phishing_clicados') / user.get('emails_phishing_recibidos'))
-    else:
-        usuarios_train_x.append(0)
-    usuarios_train_y.append(user.get('vulnerable'))
-print(usuarios_train_x, usuarios_train_y)
-
-with open("Logs/users_IA_predecir.json") as usersjson:
-    users = json.load(usersjson).get('usuarios')
-print(users)
-usuarios_predecir_x = []
-for user in users:
-    if user.get('emails_phishing_clicados') != 0:
-        usuarios_predecir_x.append(user.get('emails_phishing_clicados') / user.get('emails_phishing_recibidos'))
-    else:
-        usuarios_predecir_x.append(0)
-
+import numpy as np
 
 def linearRegresion():
+    with open("Logs/users_IA_clases.json") as usersjson:
+        users = json.load(usersjson).get('usuarios')
+    usuarios_train_x = []
+    usuarios_train_y = []
+    usuarios = []
+    for user in users:
+        usuarios.append(user.get('usuario'))
+        usuarios_train_x.append([user.get('emails_phishing_clicados'), user.get('emails_phishing_recibidos')])
+        usuarios_train_y.append(user.get('vulnerable'))
+    with open("Logs/users_IA_predecir.json") as usersjson:
+        users = json.load(usersjson).get('usuarios')
+    usuarios_predecir_x = []
+    for user in users:
+        usuarios_predecir_x.append([user.get('emails_phishing_clicados'), user.get('emails_phishing_recibidos')])
+
     regr = linear_model.LinearRegression()
     regr.fit(usuarios_train_x, usuarios_train_y)
     users_pred = regr.predict(usuarios_predecir_x)
-    print(users_pred)
+    for user in users_pred:
+        if user >= 0.5:
+            users_pred[np.argmax(users_pred == user)] = 1
+        else:
+            users_pred[np.argmax(users_pred == user)] = 0
+    fig = plt.figure()
+
+    plt.scatter(usuarios_predecir_x, users_pred)
+    plt.plot(usuarios_predecir_x, users_pred)
+    plt.grid()
 
 
 linearRegresion()
